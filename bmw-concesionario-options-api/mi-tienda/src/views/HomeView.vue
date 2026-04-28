028bab7b8b91d7bdbe28f6a5e9886cc3bbb06e94
Rodrigo Martín <rodrigomgbc@gmail.com>
2026-04-29 01:04:32 +0200

    he avanzando con la practica, he terminado el footer y he empezado con la vista de home

--- bmw-concesionario-options-api/mi-tienda/src/views/HomeView.vue
+++ bmw-concesionario-options-api/mi-tienda/src/views/HomeView.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    <h1>Home</h1>
-  </div>
+  <v-contanier fluid class="pa-0">
+
+    <!-- Hero-->
+     <v-sheet color="blue-darken-3" class="text-white text-center py-16 px-4">
+      <h1 class="text-h3 font-weight-bold mb-4">Bienvenido a Concesionario BMW</h1>
+      <p class="text-h6 mb-6">Descubre nuestra gama de vehículos de alta gama</p>
+      <v-btn color="white" variant="flat" to="/catalogo">Ver catálogo</v-btn>
+     </v-sheet>
+
+  </v-contanier>
 </template>
 
 <script setup>
